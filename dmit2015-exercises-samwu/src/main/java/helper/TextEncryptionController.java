925fbd09bc65e3e54ab39286595414119a99e52a
Sam Wu <swu@nait.ca>
2016-04-08 21:05:31 -0600

    Added example on using Spring Security for hashing password, encrypting text, decrypting text, and configuring web app to use spring security

---
+++ dmit2015-exercises-samwu/src/main/java/helper/TextEncryptionController.java
@@ -0,0 +1,53 @@
+package helper;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.security.crypto.keygen.KeyGenerators;
+
+@Named
+@ViewScoped
+public class TextEncryptionController implements java.io.Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private final String salt = KeyGenerators.string().generateKey();
+	private final String password = "Password2015";
+	
+	public void doEncryptText() {
+		TextEncryptor encryptor = Encryptors.text(password, salt);
+		String encryptedText = encryptor.encrypt(text);
+		String message = String.format("The encrypted text for %s is %s",
+				text, encryptedText);
+		JSFHelper.addInfoMessage(message);
+	}
+	
+	public void doDecryptText() {
+		TextEncryptor encryptor = Encryptors.text(password, salt);
+		String decryptedText = encryptor.decrypt(text);
+		String message = String.format("The decrypted text for %s is %s",
+				text, decryptedText);
+		JSFHelper.addInfoMessage(message);
+	}
+	
+	
+	private String text;	// generate getter/setter
+
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public void doEncodePassword() {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode(text);
+		String message = String.format("The hash password for %s is: %s", 
+				text, hashedPassword);
+		JSFHelper.addInfoMessage(message);
+	}
+	
+}
